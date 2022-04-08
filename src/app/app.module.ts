9e34515333cc152134509a8a93fe35c40cbe74ad
Eric Leonard <lenny2k12@gmail.com>
2022-04-08 20:52:13 +0100

    Some Work Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
+import { PokemonListComponent } from './pokemon2/pokemon-list/pokemon-list.component';
+import { PokemonRowComponent } from './pokemon2/pokemon-row/pokemon-row.component';
+import { PokemonDetailsComponent } from './pokemon2/pokemon-details/pokemon-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonComponent,
+    PokemonListComponent,
+    PokemonRowComponent,
+    PokemonDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pokemon2/pokemon-row/pokemon-row.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-row',
+  templateUrl: './pokemon-row.component.html',
+  styleUrls: ['./pokemon-row.component.css']
+})
+export class PokemonRowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
